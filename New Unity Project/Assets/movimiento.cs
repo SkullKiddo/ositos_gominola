696ff6d808868be5a4e6e08f042d5d04754ebb56
Unknown <gabrielpuigdemunt@gmail.com>
2016-10-08 05:12:34 +0200

    pelota se mueve
    intento de camara no hace nada

---
+++ New Unity Project/Assets/movimiento.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimiento : MonoBehaviour {
+
+    // Use this for initialization
+    public float GRAVEDAD;
+    public float MAXspeed;
+    private Rigidbody rb;
+
+    void DireccionMovimiento(ref float speed, ref Vector3 dir) {
+        dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        speed = MAXspeed;
+    }
+
+    void Start () {
+        Physics.gravity = new Vector3(0, -GRAVEDAD, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float speed = 0.0F;
+        Vector3 dir = new Vector3(0, 0, 0);
+        DireccionMovimiento(ref speed, ref dir); //aqui se pilla el input para poder cambiarlo para moverlo al movil cambiando la
+        if (speed > MAXspeed) speed = MAXspeed;
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(dir * speed);
+
+
+
+    }
+}
---
+++ New Unity Project/Assets/seguimiento.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class seguimiento : MonoBehaviour {
+
+    // Use this for initialization
+    private Vector3 offset;
+    public GameObject player = GameObject.Find("Player");
+    void Start () {
+        offset = transform.position - player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+    }
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+    }
+}
