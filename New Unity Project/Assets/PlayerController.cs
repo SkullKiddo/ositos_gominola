e194816b8f510c01e0087ece0e8f6991271ad66a
Unknown <paolapardoat@gmail.com>
2016-10-08 10:04:25 +0200

    Paola no sab usar git
    en serio, no sabe... paola plz

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
-    public int velocity; 
+    public int velocity;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+        winText.text = "";
+        count = 0;
+        SetCountText();
 	}
 
     void FixedUpdate()
@@ -18,6 +27,22 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * velocity);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 12) winText.text = "You win!";
     }
 		
 }
